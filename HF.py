af70757c8f41739425626b9074b183eb1fa98961
Blessing Egharevba <60974441+blennyblex@users.noreply.github.com>
2021-04-07 19:13:39 +0100

    Update HF.py

--- HF.py
+++ HF.py
@@ -10,17 +10,39 @@ df1 = df.drop("DEATH_EVENT", 1)
 
 # try: 
 
+st.sidebar.header("User Medical Records")
+st.sidebar.subheader("Please enter your credentials here")
+
+
+st.header("HEART_FAILURE PREDICTION")
+st.text("This app predicts the likelihood of a person having an Heart Attack")
+
+st.subheader("Insert the appropriate values using the input buttons by the left providing the following details: ")
+st.text("""     
+        1. age
+        2. anaemia
+        3. creatinine_phosphokinase
+        4. diabetes
+        5. ejection_fraction
+        6. high_blood_pressure
+        7. platelets
+        8. serum_creatinine
+        9. serum_sodium
+        10.sex
+        11.smoking
+        12.time"""
+)
+
 def user_input():
-    st.header("Heart_Failure prediction")
-    age = st.sidebar.number_input("What is your age")
+    age = st.sidebar.number_input("What is your age?")
     anaemia = st.sidebar.selectbox("Do you have anaemia? ", (0, 1))
-    creatinine_phosphokinase = st.sidebar.number_input("What is your level of creatinine_phosphokinase")
+    creatinine_phosphokinase = st.sidebar.number_input("What is your creatinine_phosphokinase level?")
     diabetes = st.sidebar.selectbox("Do you have diabetes? ", (0, 1))
-    ejection_fraction = st.sidebar.number_input("What is your ejection fraction")
+    ejection_fraction = st.sidebar.number_input("What is your ejection fraction?")
     high_blood_pressure = st.sidebar.selectbox("Do you have high blood pressure? ", (0, 1))
-    platelets = st.sidebar.number_input("What is your Blood Platelets count")
-    serum_creatinine = st.sidebar.number_input("What is your body level of serum_creatinine")
-    serum_sodium = st.sidebar.number_input("What is your body level of serum sodium ")
+    platelets = st.sidebar.number_input("What is your Blood Platelets count?")
+    serum_creatinine = st.sidebar.number_input("What is your body serum_creatinine level?")
+    serum_sodium = st.sidebar.number_input("What is your body level of serum sodium?")
     sex = st.sidebar.selectbox("What is your sex? Note: F = 1, M = 0", (0,1))
     smoking = st.sidebar.selectbox("Do you smoke?", (0, 1))
     time = st.sidebar.number_input("How many times have you gone for appointments?")
@@ -51,16 +73,17 @@ def scaling():
 
 df_ = scaling()
 
-st.subheader(body = "Scaled User input")
-st.dataframe(df_)
 
 prediction = model.predict(df_)
-st.subheader(body = "prediction")
+st.subheader(body = "Prediction")
 
 predict = pd.DataFrame({"DEATH_EVENT": prediction})
 st.dataframe(predict)
 
+if prediction == 0:
+    st.write("This Patient is less likely to have an heart attack!!")
+else:
+    st.write("This Patient is suffering from Heart attack!!")
+
 # except ValueError:
 #     st.write("Please input correct values by the left")
-
-
